d6c2dc14cbffa32ff70d5115f66fc771395356ea
Anamika-1629 <anamikarai835@gmail.com>
2025-08-14 23:29:55 +0530

    day 10 updated

--- Day10/NextRound.py
+++ Day10/NextRound.py
@@ -1,17 +1,22 @@
 class Solution:
+    @staticmethod
     def round(l, k):
         count = 0
         for i in range(len(l)):
-            if l[i] >= k:
+            if l[i] >= k and l[i] > 0:
                 count += 1
         return count
+
 
 def main():
     n, k = map(int, input().split())
     l = list(map(int, input().split()))
+
+    k = l[k - 1]
     
     result = Solution.round(l, k)
     print(result)
+
 
 if __name__ == "__main__":
     main()
--- Day10/Solution.java
+++ Day10/Solution.java
@@ -1,27 +1,30 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Solution{
-    public static int round(ArrayList<Integer> arr, int k){
+public class Solution {
+    public static int round(ArrayList<Integer> arr, int k) {
         int count = 0;
-        for(int j = 0; j<arr.size(); j++){
-            if (arr.get(j) >= k){
-                count ++;
+        for (int j = 0; j < arr.size(); j++) {
+            if (arr.get(j) >= k && arr.get(j) > 0) {
+                count++;
             }
         }
         return count;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        ArrayList <Integer> arr = new ArrayList<>();
-
-        for (int i = 0; i<n; i++){
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             arr.add(sc.nextInt());
         }
-        Solution s1 = new Solution();
-        System.out.println(s1.round(arr,k));
+        k = arr.get(k - 1);
+
+        System.out.println(round(arr, k));
+        sc.close();
     }
 }
