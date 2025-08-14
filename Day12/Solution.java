3c6097a1d5bf5465180c54de051a1d691ff4f415
Anamika-1629 <anamikarai835@gmail.com>
2025-08-14 23:33:42 +0530

    day 12 updated

--- Day12/Solution.java
+++ Day12/Solution.java
@@ -1,27 +1,28 @@
-
 import java.util.Scanner;
 
 public class Solution {
-    public static int compare(String a, String b){
+
+    public static int compare(String a, String b) {
         String t1 = a.toLowerCase();
-        String t2 = b.toLowerCase(); 
-        
-        if (t1.compareTo(t2) < 0){
+        String t2 = b.toLowerCase();
+
+        if (t1.compareTo(t2) < 0) {
             return -1;
-        }
-        else if (t1.compareTo(t2) > 0){
+        } else if (t1.compareTo(t2) > 0) {
             return 1;
-        }
-        else{
+        } else {
             return 0;
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        String b = sc.nextLine();
+
+        String a = sc.nextLine().trim();
+        String b = sc.nextLine().trim();
+
         sc.close();
-        
-        System.out.println(compare(a,b));
+
+        System.out.println(compare(a, b));
     }
 }
